00e6139bbea207a4b4d11c7f6209ebdb447ed474
CarlUdo <carl@wbsweden.com>
2024-10-19 12:01:43 +0200

    Move script to index.html and remove image links from card component accordingly

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,6 @@ export const greet = () => {
   return "Hello World from School of Applied Technology 👋🏼";
 };
 
-export const setBackgroundImageDynamically = () => {
-  const cards = document.querySelectorAll('.card__image');
-
-  cards.forEach((card: HTMLDivElement, index) => {
-    if (window.innerWidth < 768) {
-      card.style.backgroundImage = `url('https://picsum.photos/370/390?random=${index + 1}')`;
-    } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
-      card.style.backgroundImage = `url('https://picsum.photos/340/250?random=${index + 1}')`;
-    } else {
-      card.style.backgroundImage = `url('https://picsum.photos/320/200?random=${index + 1}')`;
-    }    
-  });
-}
-
 const main = () => {
   console.log(greet());
 };
